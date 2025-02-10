2100a8a6eb638031c5b8b3dbc8246e99428ca97e
thedragonbros <quackahh123@gmail.com>
2025-02-10 14:49:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ export default {
             const [client, server] = new WebSocketPair();
             server.accept();
 
+            // Keep track of connected clients
+            const clients = [];
+
+            clients.push(server);
+
             server.addEventListener("message", event => {
-                server.send(event.data);
+                // Broadcast the message to all connected clients
+                clients.forEach(client => client.send(event.data));
+            });
+
+            server.addEventListener("close", () => {
+                // Remove client from the list when they disconnect
+                const index = clients.indexOf(server);
+                if (index !== -1) {
+                    clients.splice(index, 1);
+                }
             });
 
             return new Response(null, { status: 101, webSocket: client });
